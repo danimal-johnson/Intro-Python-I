5e85ad9a0fc16ed3a0cc51336ee58adea5eb30cc
Dan Johnson <danimal.johnson@gmail.com>
2020-04-08 11:05:48 -0700

    Add/update exercises 8, 9, 11, 13, 15.

--- src/11_args.py
+++ src/11_args.py
@@ -6,13 +6,24 @@
 
 # YOUR CODE HERE
 
+
+def f1(a, b):
+    return a + b
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+
+def f2(*args):
+    sum = 0
+    for x in args:
+        sum += x
+    return sum
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -29,7 +40,12 @@ print(f2(a))    # Should print 22
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+
+def f3(a, b=1)
+
+
+return a + b
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
